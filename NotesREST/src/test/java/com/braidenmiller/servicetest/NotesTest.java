7751fa270be55b6e4c9f844fcceef28e4a135b57
Braiden Miller <braidenjamesmiller@gmail.com>
2020-03-17 20:46:17 -0600

    updates to tests and updated frontend

--- NotesREST/src/test/java/com/braidenmiller/servicetest/NotesTest.java
+++ NotesREST/src/test/java/com/braidenmiller/servicetest/NotesTest.java
@@ -1,65 +1,83 @@
 package com.braidenmiller.servicetest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Set;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.braidenmiller.notesapp.NotesRestApplication;
 import com.braidenmiller.notesapp.entities.Note;
-import com.braidenmiller.notesapp.entities.User;
-import com.braidenmiller.notesapp.services.NoteService;
-import com.braidenmiller.notesapp.services.UserService;
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = NotesTest.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = NotesTest.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = NotesRestApplication.class)
+@AutoConfigureMockMvc
 class NotesTest {
 
 	private Note note;
 
-	@MockBean
-	private NoteService noteService;
+//	@MockBean
+//	private NoteService noteService;
+//	
+//	@MockBean
+//	private UserService userService;
 	
-	@MockBean
-	private UserService userService;
+	@Autowired
+    private MockMvc mockMvc;
 	
+	private static final String RESTURL="/api/";
 
-//	@BeforeEach
-//	void setUp() throws Exception {
-//		
-//		
-//		
-//	}
-//
-//	@AfterEach
-//	void tearDown() throws Exception {
-//		
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		
 //		noteService.destroy("test", note.getId());
-//	}
+	}
 
 	@Test
 	void test_index() {
 		
-		User testUser = userService.show(4);
-		note = new Note();
-		note.setDetails("Test Dtails");
-		note.setTitle("Test Title");
-		note.setCompleted(false);
-		note.setUser(testUser);
+		try {
+			this.mockMvc.perform( MockMvcRequestBuilders
+			           .get(RESTURL+"index"))
+			               .andDo(print())
+			               .andExpect(status().isOk());
+		} catch (Exception e) {
+			assertTrue(false);
+		}     
 		
-		note = noteService.create("test", note);
-		
-		Set<Note> testNotes = noteService.index("test");
-		
-		assertTrue(testNotes.size() == 1);
+//		User testUser = userService.show(4);
+//		note = new Note();
+//		note.setDetails("Test Dtails");
+//		note.setTitle("Test Title");
+//		note.setCompleted(false);
+//		note.setUser(testUser);
+//		
+//		note = noteService.create("test", note);
+//		
+//		Set<Note> testNotes = noteService.index("test");
+//		
+//		assertTrue(testNotes.size() == 1);
 		
 	}
 //	@Test
